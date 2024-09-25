c3a6c672565a9b49d7afaeed56b67433306a4a01
PhiBuck <168071966+PhiBuck@users.noreply.github.com>
2024-09-25 12:15:54 +0200

    project structure

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,30 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './shared/header/header.component';
+import { HeroSectionComponent } from './hero-section/hero-section.component';
+import { AboutMeComponent } from './about-me/about-me.component';
+import { SkillsComponent } from './skills/skills.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { ReferencesComponent } from './references/references.component';
+import { ContactComponent } from './contact/contact.component';
+import { FooterComponent } from './shared/footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [
+    CommonModule,
+    RouterOutlet,
+    HeaderComponent,
+    HeroSectionComponent,
+    AboutMeComponent,
+    SkillsComponent,
+    ProjectsComponent,
+    ReferencesComponent,
+    ContactComponent,
+    FooterComponent,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
