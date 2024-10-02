30b85f19e63ba6b2005d7afc66f55225d4c2105b
PhiBuck <168071966+PhiBuck@users.noreply.github.com>
2024-10-02 16:38:40 +0200

    project sections item list

--- src/app/projects/projects.component.ts
+++ src/app/projects/projects.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Project } from '../models/project-list-interface';
 
 @Component({
   selector: 'app-projects',
@@ -8,5 +9,28 @@ import { Component } from '@angular/core';
   styleUrl: './projects.component.scss'
 })
 export class ProjectsComponent {
+
+  projects: Project[] = [
+    {
+      id: 1,
+      title: 'Join',
+      usedSkills: ['JavaScript', 'HTML', 'CSS', 'Figma']
+    },
+    {
+      id: 2,
+      title: 'El Pollo Loco',
+      usedSkills: ['JavaScript', 'HTML', 'CSS', 'OOP']
+    },
+    {
+      id: 3,
+      title: 'Portfolio Website',
+      usedSkills: ['Angular', 'TypeScript', 'HTML', 'SCSS', 'Figma']
+    },
+    {
+      id: 4,
+      title: 'Pokedex',
+      usedSkills: ['JavaScript', 'HTML', 'CSS', 'API']
+    }
+  ];
 
 }
