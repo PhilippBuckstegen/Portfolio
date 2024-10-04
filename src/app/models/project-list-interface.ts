f34eb96bf432adc5ba7026972d3698e405820c63
PhiBuck <168071966+PhiBuck@users.noreply.github.com>
2024-10-04 18:06:25 +0200

    Overlay Projects Section

--- src/app/models/project-list-interface.ts
+++ src/app/models/project-list-interface.ts
@@ -1,6 +1,8 @@
 export interface Project {
     id: number;
+    count: string;
     title: string;
     usedSkills: string[];
     image: string;
+    bgImage: string;
   }
--- src/app/projects/projects.component.ts
+++ src/app/projects/projects.component.ts
@@ -1,40 +1,62 @@
 import { Component } from '@angular/core';
 import { Project } from '../models/project-list-interface';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-projects',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './projects.component.html',
   styleUrl: './projects.component.scss'
 })
 export class ProjectsComponent {
 
+  isOverlayOpen = false;
+  selectedProject: Project | null = null;
+
   projects: Project[] = [
     {
       id: 1,
+      count: '01',
       title: 'Join',
       usedSkills: ['JavaScript', 'HTML', 'CSS', 'Figma'],
       image: '/assets/img/join-image-project.png',
+      bgImage: '/assets/img/bg-image-project.png',
     },
     {
       id: 2,
+      count: '02',
       title: 'El Pollo Loco',
       usedSkills: ['JavaScript', 'HTML', 'CSS', 'OOP'],
-      image: '/assets/img/join-image-project.png',
+      image: '/assets/img/pollo-loco-image-project.png',
+      bgImage: '/assets/img/bg-image-project.png',
     },
     {
       id: 3,
+      count: '03',
       title: 'Portfolio Website',
       usedSkills: ['Angular', 'TypeScript', 'HTML', 'SCSS', 'Figma'],
       image: '/assets/img/join-image-project.png',
+      bgImage: '/assets/img/bg-image-project.png',
     },
     {
       id: 4,
+      count: '04',
       title: 'Pokedex',
       usedSkills: ['JavaScript', 'HTML', 'CSS', 'API'],
       image: '/assets/img/join-image-project.png',
+      bgImage: '/assets/img/bg-image-project.png',
     }
   ];
+
+  openOverlay(project: Project) {
+    this.selectedProject = project;
+    this.isOverlayOpen = true;
+  }
+
+  closeOverlay() {
+    this.isOverlayOpen = false;
+    this.selectedProject = null;
+  }
 
 }
