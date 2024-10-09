02729231d97e8b53da914d79cb77bc0bfa147c8d
PhiBuck <168071966+PhiBuck@users.noreply.github.com>
2024-10-09 23:34:03 +0200

    reference section carousel

--- src/app/references/references.component.ts
+++ src/app/references/references.component.ts
@@ -8,16 +8,58 @@ import { Reference } from '../models/reference-list-interface';
   standalone: true,
   imports: [CommonModule, FormsModule],
   templateUrl: './references.component.html',
-  styleUrl: './references.component.scss'
+  styleUrls: ['./references.component.scss'],
 })
 export class ReferencesComponent {
-
   references: Reference[] = [
     {
       text: 'Test, dies ist eine gute Referenz',
-      name: 'Lyonel',
-      status: 'Team-Partner'
+      name: 'Lyonel Berzen',
+      status: 'Team-Partner',
+    },
+    {
+      text: 'This is an amazing colleague who helped me a lot.',
+      name: 'Heiko Nickel',
+      status: 'Frontend Developer',
+    },
+    {
+      text: 'Philipp is a great team player and always delivers on time.',
+      name: 'Marvin Dresp',
+      status: 'Project Manager',
+    },
+    {
+      text: 'Philipp has excellent skills and is a strong communicator.',
+      name: 'Fabion Latifi',
+      status: 'Team-Partner',
     },
   ];
 
+  currentIndex = 1;
+
+  nextReference() {
+    this.currentIndex = (this.currentIndex + 1) % this.references.length;
+  }
+
+  prevReference() {
+    this.currentIndex =
+      (this.currentIndex - 1 + this.references.length) % this.references.length;
+  }
+
+  getLeftIndex(): number {
+    return (
+      (this.currentIndex - 1 + this.references.length) % this.references.length
+    );
+  }
+
+  getRightIndex(): number {
+    return (this.currentIndex + 1) % this.references.length;
+  }
+
+  isVisible(index: number): boolean {
+    return (
+      index === this.currentIndex ||
+      index === this.getLeftIndex() ||
+      index === this.getRightIndex()
+    );
+  }
 }
