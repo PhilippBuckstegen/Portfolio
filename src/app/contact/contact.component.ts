5c98310cc046dcfdff9654c79ac48b2a3c3ca0e7
PhiBuck <168071966+PhiBuck@users.noreply.github.com>
2024-10-10 18:11:59 +0200

    added contact form

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,12 +1,46 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { catchError } from 'rxjs/operators';
+import { throwError } from 'rxjs';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-contact',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, FormsModule, ReactiveFormsModule, HttpClientModule],
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.scss'
 })
 export class ContactComponent {
+  contactForm: FormGroup;
 
+  constructor(private fb: FormBuilder, private http: HttpClient) {
+    this.contactForm = this.fb.group({
+      name: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      message: ['', Validators.required],
+      privacyPolicy: [false, Validators.requiredTrue],
+    });
+  }
+
+  async onSubmit() {
+    if (this.contactForm.valid) {
+      const formData = this.contactForm.value;
+      try {
+        const response = await this.http
+          .post('https://formspree.io/YOUR_FORM_ID', formData)
+          .pipe(
+            catchError((error) => {
+              console.error('Error submitting form', error);
+              return throwError(error);
+            })
+          )
+          .toPromise();
+        console.log('Form submitted successfully', response);
+      } catch (error) {
+        console.error('Submission failed', error);
+      }
+    }
+  }
 }
--- src/app/references/references.component.ts
+++ src/app/references/references.component.ts
@@ -15,22 +15,22 @@ export class ReferencesComponent {
     {
       text: 'Philipp is an extremely reliable and friendly team colleague. He always comes up with great ideas and contributes significantly to the success of our team. His positive attitude and creativity are a real asset to us all.',
       name: 'Lyonel Berzen',
-      status: 'Team-Partner',
+      status: 'Team Partner',
     },
     {
       text: 'Philipp played a key role in the success of our project with his dedication and ability to manage all tasks effectively. His expertise in CSS, JavaScript, and HTML, along with his valuable input, enriched the team. His friendly and motivating nature also helped maintain high team morale.',
       name: 'Heiko Nickel',
-      status: 'Team-Partner',
+      status: 'Team Partner',
     },
     {
       text: 'Philipp proved to be a highly competent and dedicated colleague in programming. His quick understanding and creative problem-solving were impressive. Always helpful, he provided focused support on challenging tasks. His commitment ensured our projects were completed successfully and on time, making his solution-oriented approach and team spirit invaluable.',
       name: 'Marvin Dresp',
-      status: 'Team-Partner',
+      status: 'Team Partner',
     },
     {
       text: 'Philipp was a natural leader who fostered a positive, collaborative environment. He assigned tasks based on team strengths and often tackled the toughest challenges himself. His ability to find practical solutions to complex problems and his attention to detail not only improved project quality but also inspired the team to excel.',
       name: 'Fabion Latifi',
-      status: 'Team-Partner',
+      status: 'Team Partner',
     },
   ];
 
