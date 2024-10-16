c85286367b7b5573b89e570efeab248a9547b217
PhiBuck <168071966+PhiBuck@users.noreply.github.com>
2024-10-16 12:11:48 +0200

    added privacy policy

--- src/app/legal-notice/legal-notice.component.ts
+++ src/app/legal-notice/legal-notice.component.ts
@@ -6,16 +6,17 @@ import { Router, NavigationEnd } from '@angular/router';
   standalone: true,
   imports: [],
   templateUrl: './legal-notice.component.html',
-  styleUrl: './legal-notice.component.scss'
+  styleUrl: './legal-notice.component.scss',
 })
 export class LegalNoticeComponent implements OnInit {
-
   constructor(private router: Router) {}
 
   ngOnInit(): void {
     this.router.events.subscribe((event) => {
       if (event instanceof NavigationEnd) {
-        window.scrollTo(0, 0);
+        if (event.url === '/legal-notice' || '/privacy-policy') {
+          window.scrollTo({ top: 0, behavior: 'smooth' });
+        }
       }
     });
   }
